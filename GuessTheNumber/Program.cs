e602e52c53c9cf4d9e157fc7110ae656dec41835
MARIAN <MARIAN@DESKTOP-LJO9UPV>
2019-12-04 20:42:23 +0200

    Add project files.

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int numberToGuess = random.Next(100);
+            int userGuess = 0;
+
+            while (userGuess != numberToGuess)
+            {
+                Console.Write("Enter your number:");
+                int.TryParse(Console.ReadLine(), out userGuess);
+
+                if (userGuess > numberToGuess)
+                {
+                    Console.WriteLine("{0} is too high!", userGuess);
+                }
+                else if (userGuess < numberToGuess)
+                {
+                    Console.WriteLine("{0} is too small!", userGuess);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is right!", userGuess);
+                }
+
+            }
+        }
+    }
+}
